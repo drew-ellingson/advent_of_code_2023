647efa100cd41c7c2c914f10f3eb5402a6b45250
drew ellingson <drew.ellingson@gmail.com>
2023-12-31 09:30:09 -0800

    day 8 p2. doin some math

--- day_08/soln.py
+++ day_08/soln.py
@@ -1,6 +1,7 @@
-from dataclasses import dataclass, field
-from typing import List, Dict
+from dataclasses import dataclass
+from typing import List, Dict, Tuple
 import re
+import math
 
 
 @dataclass
@@ -14,22 +15,31 @@ class Graph:
     nodes: Dict[str, Node]
     instrs: List[str]
 
-    def get_steps(self, start_nodes=["AAA"], end_nodes=["ZZZ"]) -> int:
-        curr_nodes = start_nodes
+    def get_steps(self, start_node: str = "AAA", p2: bool = False) -> int:
+        curr_node = start_node
         curr_steps = 0
-        while not all(a in end_nodes for a in curr_nodes):
+
+        while not curr_node == "ZZZ" if not p2 else not curr_node.endswith("Z"):
             curr_instr = self.instrs[curr_steps % len(self.instrs)]
             if curr_instr == "L":
-                curr_nodes = [self.nodes[a].left for a in curr_nodes]
+                curr_node = self.nodes[curr_node].left
             else:
-                curr_nodes = [self.nodes[a].right for a in curr_nodes]
+                curr_node = self.nodes[curr_node].right
             curr_steps += 1
-            if curr_steps % 100000 == 1:
-                print(f"At step: {curr_steps}")
+
         return curr_steps
 
+    def all_get_steps(self) -> int:
+        """
+        assuming periodicity immmediately with no starting lag. not sure this is generally true
+        but the approach works for this problem.
+        """
+        start_nodes = {k: v for k, v in self.nodes.items() if k.endswith("A")}
+        steps = {k: self.get_steps(start_node=k, p2=True) for k in start_nodes}
+        return math.lcm(*steps.values())
 
-def parse_node(node):
+
+def parse_node(node: Node) -> Tuple[str, Node]:
     label, children = node.split(" = ")
     children = re.sub(r"\(|\)", "", children)
     left, right = children.split(", ")
@@ -45,9 +55,5 @@ if __name__ == "__main__":
 
     graph = Graph(nodes, instrs)
 
-    start_nodes = {k: v for k, v in nodes.items() if k.endswith("A")}
-    end_nodes = {k: v for k, v in nodes.items() if k.endswith("Z")}
-
-    print(
-        f"P2 Soln is: {graph.get_steps(start_nodes=start_nodes, end_nodes = end_nodes)}"
-    )
+    print(f"P1 Soln is: {graph.get_steps()}")
+    print(f"P2 Soln is: {graph.all_get_steps()}")
